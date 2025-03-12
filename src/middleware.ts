22ccc21345da431a3b033f5a780d9290c682755c
Pavel <45739285+Nazzemax@users.noreply.github.com>
2025-03-12 09:52:05 +0600

    attempt2 fix language in prod

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,6 +14,7 @@ export const config = {
 
         // Enable redirects that add missing locales
         // (e.g. `/pathnames` -> `/en/pathnames`)
+        "/services/:path*",
         "/((?!_next|_vercel|.*\\..*).*)",
     ],
 };
