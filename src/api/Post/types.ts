3a7d616120a8b151217cf9e90694832705774af2
Pavel <45739285+Nazzemax@users.noreply.github.com>
2025-03-13 11:09:21 +0600

    fix:fixed image not showing i detail case

--- src/api/Post/types.ts
+++ src/api/Post/types.ts
@@ -8,7 +8,7 @@ export interface Post {
     tags: { tags: string; }[]
     social_media: { title: string; logo: string; subscribers: string; }[]
     tasks?: { title: string; description: string; }[]
-    images?: { title: string; image: string; description: string; }[]
+    images?: { title: string; image: string[]; description: string; }[]
     results?: { header: string; title: string; description: string; }[]
     target?: { title: string; description: string; }[]
 }
--- src/components/organisms/case-images/index.tsx
+++ src/components/organisms/case-images/index.tsx
@@ -2,25 +2,32 @@ import { Heading } from "@/components/atoms/heading";
 import { CaseImageItem } from "@/components/molecules/case-image-item";
 
 interface CaseImagesProps {
-    images: { title: string; image: string; description: string; }[];
-}
+    images: {
+      title: string;
+      image: string[]; // Updated: image is now an array of strings
+      description: string;
+    }[];
+  }
 
-export const CaseImages = ({
-    images,
-}: CaseImagesProps) => {
+export const CaseImages = ({ images }: CaseImagesProps) => {
+
+    console.log(images[0])
     return (
         <article className="max-w-[1120px] m-auto my-8 md:my-16 px-5 flex flex-col lg:flex-row gap-y-5 w-full">
-            <Heading as="h4" className="font-normal uppercase text-base md:text-lg text-gray2 min-w-[320px]">{images[0]?.title}</Heading>
+            <Heading as="h4" className="font-normal uppercase text-base md:text-lg text-gray2 min-w-[320px]">
+                {images[0]?.title}
+            </Heading>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-                {images.map((image) => (
-                    <CaseImageItem
-                        key={image.image} 
-                        image={image.image} 
-                        description={image.description}                        
-                    />
-                    
+                {images?.map((image) => (
+                    image?.image?.map((img) => (
+                        <CaseImageItem
+                            key={img}
+                            image={img}
+                            description={image.description}
+                        />  
+                    ))
                 ))}
             </div>
         </article>
-    )
-}
+    );
+};
