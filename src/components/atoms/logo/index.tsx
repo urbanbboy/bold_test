2a6fdac9250db24eea33b44c01f90375fd9c8b37
urbanbboy <talant04.asanov@gmail.com>
2025-03-14 14:03:02 +0600

    fix: logo font

--- src/components/atoms/logo/index.tsx
+++ src/components/atoms/logo/index.tsx
@@ -1,24 +1,27 @@
 import { Link } from '@/i18n/navigation'
 import localFont from 'next/font/local'
 
-const akrobatFontBlack = localFont({
-    src: "../../../../public/fonts/Akrobat-Black.otf",
-})
+// const akrobatFontBlack = localFont({
+//     src: "../../../../public/fonts/Akrobat-Black.otf",
+// })
 
+const akrobatFontExtraBold = localFont({
+    src: "../../../../public/fonts/Akrobat-ExtraBold.otf",
+})
 const akrobatFontSemibold = localFont({
     src: "../../../../public/fonts/Akrobat-Bold.otf",
 })
 
 export const Logo = () => {
     return (
-        <Link href={'/home'} className={`flex items-center text-[#FF2B44] ${akrobatFontBlack.className} text-2xl md:text-3xl font-extrabold`}>
+        <Link href={'/home'} className={`flex items-center text-accent ${akrobatFontExtraBold.className} text-2xl md:text-3xl font-extrabold`}>
             <div className="flex flex-col gap-1 mr-2">
-                <div className="w-[26px] h-[8px] bg-[#FF2B44] rounded-bl-[4px] rounded-tr-[4px]"></div>
+                <div className="w-[26px] h-[8px] bg-accent rounded-bl-[4px] rounded-tr-[4px]"></div>
                 <div
-                    className={`w-[26px] h-[8px] bg-[#FF2B44] rounded-l-[4px]`}
+                    className={`w-[26px] h-[8px] bg-accent rounded-l-[4px]`}
                     style={{ clipPath: "polygon(100% 0%, 97.8% 13.62%, 94.86% 27.11%, 89.47% 39.45%, 82.25% 49.27%, 89.47% 60.82%, 94.86% 74.68%, 97.8% 87.14%, 100% 99.75%, 0% 100%, 0% 49.27%, 0% 0%)" }}
                 ></div>
-                <div className="w-[26px] h-[8px] bg-[#FF2B44] rounded-t-[4px]"></div>
+                <div className="w-[26px] h-[8px] bg-accent rounded-t-[4px]"></div>
             </div>
             <div className="relative" style={{ transform: "scaleY(0.85)" }}>
                 <span className="leading-none tracking-tight">Bold Brands</span>
