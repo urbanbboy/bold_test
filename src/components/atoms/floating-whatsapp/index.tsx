abc556e8a5218256bac04e429e576fe441df1a92
urbanbboy <talant04.asanov@gmail.com>
2025-03-19 13:40:25 +0600

    fix: whatsapp mounting

--- src/components/atoms/floating-whatsapp/index.tsx
+++ src/components/atoms/floating-whatsapp/index.tsx
@@ -1,19 +1,31 @@
 'use client';
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FloatingWhatsApp } from 'react-floating-whatsapp'
 
 const FloatingWhatsapp = () => {
-    return <FloatingWhatsApp
-        phoneNumber="+996999504444" // Номер телефона в международном формате
-        accountName="Bold Brands International"
-        notificationSound
-        chatMessage="Доброго времени суток, чем могу вам помочь?"
-        statusMessage="Онлайн"
-        darkMode
-        avatar="https://bishkek.headhunter.kg/employer-logo/6266415.png"
-        placeholder="Введите текст"
-    />
+    const [mounted, setMounted] = useState(false)
+
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) {
+        return null;
+    }
+
+    return (
+        <FloatingWhatsApp
+            phoneNumber="+996999504444" // Номер телефона в международном формате
+            accountName="Bold Brands International"
+            notificationSound
+            chatMessage="Доброго времени суток, чем могу вам помочь?"
+            statusMessage="Онлайн"
+            darkMode
+            avatar="https://bishkek.headhunter.kg/employer-logo/6266415.png"
+            placeholder="Введите текст"
+        />
+    );
 }
 
 export default FloatingWhatsapp;
